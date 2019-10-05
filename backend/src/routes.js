acfaa72c573bef7cbae6725c18482823a7320a9b
pulabio <pulabio@gmail.com>
2019-10-05 17:46:27 -0300

    feature: users' spots are listed in their dashboards

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 // refer to multer docs
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
