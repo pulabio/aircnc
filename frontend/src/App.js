e4fd36d12336477b2e9aeb405a72e1cbcffcafea
pulabio <pulabio@gmail.com>
2019-10-09 13:12:47 -0300

    receives form state + some stylistic changes from eslint and prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
+
       <div className="content">
-        <p> 
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre{' '}
+          <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail *</label>
-          <input 
+          <input
             type="email"
             id="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
-          <button className="btn" type="submit">Entrar</button>
+
+          <button className="btn" type="submit">
+            Entrar
+          </button>
         </form>
       </div>
     </div>
