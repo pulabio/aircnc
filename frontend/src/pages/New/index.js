8d521d6131a5724bafc44ea9035d04d0865273f2
pulabio <pulabio@gmail.com>
2019-10-12 00:00:08 -0300

    feature: create new spot ( still without thumbnails )

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function New() {
-  return <div />;
+  const [company, setCompany] = useState('');
+  const [techs, setTechs] = useState('');
+  const [price, setPrice] = useState('');
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="company"> EMPRESA * </label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">
+        TECNOLOGIAS * <span> (separadas por vírgula) </span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">VALOR DA DIÁRIA * (separadas por vírgula)</label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">
+        Cadastrar
+      </button>
+    </form>
+  );
 }
