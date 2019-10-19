6be14cdfaee394fe9b28faf9bc4151ed6f6f01da
pulabio <pulabio@gmail.com>
2019-10-19 00:02:44 -0300

    Navigate user to Book from SpotList component

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { withNavigation } from 'react-navigation';
 import {
   View,
   Text,
@@ -10,7 +11,7 @@ import {
 
 import api from '../services/api';
 
-export default function SpotList({ tech }) {
+function SpotList({ tech, navigation }) {
   const [spots, setSpots] = useState([]);
 
   useEffect(() => {
@@ -23,6 +24,10 @@ export default function SpotList({ tech }) {
     }
     loadSpots();
   }, []);
+
+  function handleNavigate() {
+    navigation.navigate('Book');
+  }
 
   return (
     <View style={styles.container}>
@@ -45,7 +50,7 @@ export default function SpotList({ tech }) {
             <Text style={styles.price}>
               {item.price ? `${item.price}/dia` : 'GRATUITO'}
             </Text>
-            <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <TouchableOpacity style={styles.button} onPress={handleNavigate}>
               <Text style={styles.buttonText}>Solicitar reserva</Text>
             </TouchableOpacity>
           </View>
@@ -104,3 +109,5 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
 });
+
+export default withNavigation(SpotList);
