e833e871394d0a175baa00e147e2dbccbde566d0
pulabio <pulabio@gmail.com>
2019-10-19 17:04:04 -0300

    Book component structure and style

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,11 +1,71 @@
-import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  AsyncStorage,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function Book({ navigation }) {
+  const [date, setDate] = useState('');
   const spotID = navigation.getParam('spotId');
+
+  function handleSubmit() {}
+
   return (
-    <SafeAreaView>
-      <Text> {spotID} </Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>Data de interesse:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Qual data você quer reservar?"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+      <TouchableOpacity style={styles.button} onPress={() => handleSubmit()}>
+        <Text style={styles.buttonText}>Solicitar reserva</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, styles.cancelButton]}
+        onPress={() => handleSubmit()}
+      >
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 30,
+  },
+
+  label: {
+    marginTop: 30,
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+  },
+
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+  cancelButton: {
+    marginTop: 10,
+    backgroundColor: '#ccc',
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
