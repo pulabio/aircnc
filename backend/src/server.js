f2d10e2d453b1ecdde73d5c24ed0c69d952ad5ab
pulabio <pulabio@gmail.com>
2019-10-20 18:04:34 -0300

    stablishes socket connections between server and webapp

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,13 @@ const databaseConfig = require('./config/database');
 
 const routes = require('./routes');
 
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+// TODO: change to Redis
+const connectedUsers = {};
+
 mongoose.connect(
   `mongodb+srv://${databaseConfig.username}:${databaseConfig.password}@cluster0-cihbs.mongodb.net/semana09?retryWrites=true&w=majority`,
   {
@@ -17,14 +24,18 @@ mongoose.connect(
   }
 );
 
-const app = express();
-const server = http.Server(app);
-const io = socketio(server);
-
 io.on('connection', socket => {
-  console.log('usuário conectado', socket.id);
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
 });
 
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 // app.use(cors({origin: 'http://localhost:3333'})) only allow connections from this address
 app.use(cors());
 app.use(express.json());
