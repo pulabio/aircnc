119ff453c5f974ad862c021beb8e98cd10fa565f
pulabio <pulabio@gmail.com>
2019-10-21 23:50:39 -0300

    User gets realtime notification of booking response

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
 import {
+  Alert,
   StyleSheet,
   Text,
   Image,
@@ -14,6 +16,23 @@ import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.0.10:3333', {
+        query: { user_id },
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? 'APROVADA' : 'REJEITADA'
+          }`
+        );
+      });
+    });
+  }, []);
+
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storageTechs => {
       const techArrays = storageTechs.split(',').map(tech => tech.trim());
